5f26e9bf59c04f633a178098d92602c61cbc3bc4
korytkoBT <korytko07@gmail.com>
2015-12-06 04:48:41 +0000

    not tidyied up but works twoFoldLeft eager

--- Pwr.BT.Matchers/Tests/Test_ListOfList.fs
+++ Pwr.BT.Matchers/Tests/Test_ListOfList.fs
@@ -1,21 +1,19 @@
-﻿namespace Pwr.BT.Matchers
+﻿namespace Pwr.BT.Test
     module Test_ListOfLists =
     
-    open System
-    open System.Collections
     open NUnit.Framework
-    open NUnit.Framework.Constraints
-    open Pwr.BT.Matchers.ListOfLists
+    open Pwr.BT.Collections.List
+    open Pwr.BT.Collections.List.Oper
     
     [<Test>]
-    let TestOfFlattingListMatcher() =
+    let Oper_FlattenM_True() =
       let _inputVal1 = [1;2;1]
       let _inputVal2 = [1;2;1]
       let _inputCummVal1 = [[1;2;1];[1;2;1]]
         
       let _expectedVal = [1;2;1;1;2;1]
-      let _reachedVal = flattenM ( _inputCummVal1 )
+      let _reachedVal = flattenM _inputCummVal1
     
       let _reachedLogic = _expectedVal = _reachedVal
     
-      Assert.True(_reachedLogic)  
+      Assert.True(_reachedLogic)
